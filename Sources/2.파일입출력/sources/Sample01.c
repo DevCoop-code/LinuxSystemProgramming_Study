25aaca7390d9aee627c72b7c51113475dee874b9
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-08-20 00:57:35 +0900

    feat: study about file write

--- Sources/2.파일입출력/sources/Sample01.c
+++ Sources/2.파일입출력/sources/Sample01.c
@@ -28,14 +28,29 @@ open시 O_WRONLY | O_CREATE | O_TRUNC 조합은 너무나도 일반적이라 아
 읽은 바이트 수는 unsigned long 타입 크기와 동일하며 32bit 리눅스 시스템에서는 4 byte, 64bit 리눅스 시스템에서는 8 byte임
 */
 // ssize_t read (int fd, void *buf, size_t len);
-
 /*
-read함수 호출 시 여러 에러 값
+read 함수 호출 시 여러 에러 값
 EBADF: 주어진 파일 디스크립터가 유효하지 않거나 읽기 가능한 모드로 열리지 않음
 EFAULT: buf로 전달된 포인터가 호출하는 프로세스의 주소 공간 밖에 존재
 EINVAL: 파일 디스크립터가 읽기를 허용하지 않는 객체에 맵핑
 EIO: 저수준 입출력 에러가 발생
 */
+/*
+read() 크기 제약
+POSIX size 크기 타입
+- size_t: 바이트 단위로 크기를 측정하기 위해 사용되는 값을 저장, size_t의 최대값=SIZE_MAX
+- ssize_t: 부호가 있는(signed) size_t 타입(음수는 에러를 포함하기 위해 사용), ssize_t의 최대값=SSIZE_MAX
+*/
+
+#include <unistd.h>
+/*
+파일을 쓰는 매컨니즘은 POSIX.1에 정의된 write() 시스템 콜을 사용
+count 바이트만큼 파일 디스크립터 fd가 참조하는 파일의 현재 파일 위치에 시작 지점이 buf인 내용을 기록
+fd가 표현하는 객체에 탐색 기능이 없다면 쓰기 작업은 항상 '처음'위치에서 일어남
+
+성공하면 쓰기에 성공한 바이트 수를 반환, 파일 오프셋도 같은 크기만큼 전진함
+*/
+// ssize_t write (int fd, const void *buf, size_t count);
 
 int main(void) {
     /*
@@ -142,23 +157,44 @@ int main(void) {
     Open을 호출하면서 O_NONBLOCK을 넘겨 파일 디스크립터를 논블록 모드로 열었지만 읽을 데이터가 없다면 read()호출은 블록되는 대신 -1을 반환하며 errno를 EAGAIN으로 설정
     논블록 읽기는 EAGAIN을 반드시 점검해야 함
     */
-   int BUFSIZE = 10;
-   char buf[BUFSIZE];
+//    int BUFSIZE = 10;
+//    char buf[BUFSIZE];
+//    ssize_t nr;
+
+//    start:
+//    nr = read(fd, buf, BUFSIZE);
+//    if (nr == -1) {
+//        if (errno == EINTR) {
+//            goto start;
+//        }
+//        if (errno == EAGAIN) {
+//            // 나중에 다시 씨도
+//        }
+//        else {
+//            // Error
+//        }
+//    }
+
+    /*
+    [Write]
+    */
+   int fd;
+   const char* buf = "My ship is solid!";
    ssize_t nr;
 
-   start:
-   nr = read(fd, buf, BUFSIZE);
-   if (nr == -1) {
-       if (errno == EINTR) {
-           goto start;
-       }
-       if (errno == EAGAIN) {
-           // 나중에 다시 씨도
-       }
-       else {
-           // Error
-       }
-   }
+    fd = creat("test", 0644);
+    if ((fd == -1)) {
+        perror("open");
+    }
 
+    size_t count = strlen(buf);
+    /* 'buf'에 들어 있는 문자열을 'fd'에 기록 */
+    nr = write(fd, buf, count);
+    if (nr == -1) {
+        perror("write");
+    }
+    else if(nr != count) {
+        // Error 일 가능성이 있지만, errno는 설정되지 않음
+    }
     return 0;
 }
