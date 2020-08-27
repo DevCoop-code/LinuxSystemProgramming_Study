c1fb4cabb07618d9cd04d46bd7fd219b69a40b6c
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-08-28 00:12:31 +0900

    feat: study about execvp and fork function

--- Sources/5.프로세스관리/sources/processManaging.c
+++ Sources/5.프로세스관리/sources/processManaging.c
@@ -1,12 +1,53 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
     int ret;
 
-    ret = execl ("/bin/mkdir", "mkdir", "newDir", NULL);
-    if (ret == -1) {
-        perror("execl");
-    }
+    /*
+    execl
+    */
+    // ret = execl ("/bin/mkdir", "mkdir", "newDir", NULL);
+    // if (ret == -1) {
+    //     perror("execl");
+    // }
+
+    /*
+    execvp
+    */
+    // const char* args[] = {"vi", "/Users/hangyojeong/Dev/linuxSystemProgramming/Sources/test.txt", NULL};
+    // ret = execvp("/usr/bin/vi", args);
+    // if (ret == -1) {
+    //     perror("execvp");
+    // }
+
+    /*
+    fork
+    */
+   pid_t pid;
+   pid = fork ();
+   if (pid == -1) {
+       perror("fork");
+   }
+   // Child Process
+   if (pid == 0) {
+        ret = execl ("/bin/mkdir", "mkdir", "newDir", NULL);
+        if (ret == -1) {
+            perror("execl");
+            exit(EXIT_FAILURE);
+        }
+        // const char* args[] = {"vi", "/Users/hangyojeong/Dev/linuxSystemProgramming/Sources/test.txt", NULL};
+        // ret = execvp("/usr/bin/vi", args);
+        // if (ret == -1) {
+        //     perror("execvp");
+        // }
+   } 
+//    else {
+//        while (1) {
+
+//        }
+//    }
 
     return 0;
 }
